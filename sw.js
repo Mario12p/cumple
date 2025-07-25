1e0e0acb8b59c970d5bf230d31d2952f4d6016c8
Mario <catanopitre@gmail.com>
2025-07-25 11:01:10 -0500

    Implementar carrusel de fotos y corregir rutas de cache

--- sw.js
+++ sw.js
@@ -7,8 +7,8 @@ const urlsToCache = [
     '/script.js',
     '/manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css',
-    '/iconos/icon-192x192.png',
-    '/iconos/icon-512x512.png',
+    '/iconos/icono1.png',
+    '/iconos/icono2.png',
     '/IMAGENES/foto1.jpeg',
     '/IMAGENES/foto2.jpeg',
     '/IMAGENES/foto3.jpeg'
